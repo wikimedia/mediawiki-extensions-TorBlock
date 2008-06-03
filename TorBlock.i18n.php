cce4fc407f97685f751f0e26e85af297a25c65b6
Raimond Spekking <raymond@users.mediawiki.org>
2008-06-03 18:49:59 +0000

    Localisation updates German

--- TorBlock.i18n.php
+++ TorBlock.i18n.php
@@ -14,3 +14,11 @@ $messages['en'] = array(
 	'torblock-desc' => 'Allows Tor exit nodes to be blocked from editing a wiki',
 	'torblock-blocked' => 'Your IP address, <tt>$1</tt>, has been automatically identified as a tor exit node. Editing through tor is blocked to prevent abuse.'
 );
+
+/** German (Deutsch)
+ * @author Raimond Spekking
+ */
+$messages['de'] = array(
+	'torblock-desc'    => 'Ermöglicht die Schreibsperre im Wiki für Tor-Ausgangsknoten',
+	'torblock-blocked' => 'Deine IP-Adresse <tt>$1</tt> wurde automatisch als Tor-Ausgangsknoten identifiziert. Die Seitenbearbeitung in Verbindung mit dem Tor-Netzwerk ist unerwünscht, da die Missbrauchsgefahr sehr hoch ist.'
+);
