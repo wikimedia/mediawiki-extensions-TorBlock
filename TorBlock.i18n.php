494bb47e54e1114a7a2a20ff8fb963fb21cfdde7
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-06-04 20:23:07 +0000

    Localisation updates for extension messages from Betawiki (2008-06-04 21:52 CEST)

--- TorBlock.i18n.php
+++ TorBlock.i18n.php
@@ -25,6 +25,24 @@ $messages['de'] = array(
 	'torblock-blocked' => 'Deine IP-Adresse <tt>$1</tt> wurde automatisch als Tor-Ausgangsknoten identifiziert. Die Seitenbearbeitung in Verbindung mit dem Tor-Netzwerk ist unerwünscht, da die Missbrauchsgefahr sehr hoch ist.'
 );
 
+/** Finnish (Suomi)
+ * @author Crt
+ */
+$messages['fi'] = array(
+	'torblock-blocked'   => 'IP-osoitteesi <tt>$1</tt> on automaattisesti tunnistettu Tor-verkon exit nodeksi. Muokkaaminen Tor-verkon kautta on estetty väärinkäytösten estämiseksi.',
+	'right-torunblocked' => 'Ohittaa automaattiset Tor exit node -estot',
+);
+
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'torblock-desc'      => 'Permet aux nœuds de sortie Tor d’être bloqués en écriture sur un wiki',
+	'torblock-blocked'   => 'Votre adresse ip, <tt>$1</tt>, a été identifiée automatiquement comme un nœud de sortie Tor.
+L’édition par ce moyen est bloquée pour éviter des abus.',
+	'right-torunblocked' => 'Passer au travers des blocages des nœuds de sortie Tor.',
+);
+
 /** Hebrew (עברית)
  * @author Rotem Liss
  */
@@ -38,7 +56,27 @@ $messages['he'] = array(
  * @author Siebrand
  */
 $messages['nl'] = array(
-	'torblock-desc'    => 'Maakt bewerken onmogelijk voor tor exitnodes',
-	'torblock-blocked' => 'Uw IP-adres, <tt>$1</tt>, is herkend als tor exitnode. Bewerken via tor is niet toegestaan om misbruik te voorkomen.',
+	'torblock-desc'      => 'Maakt bewerken onmogelijk voor tor exitnodes',
+	'torblock-blocked'   => 'Uw IP-adres, <tt>$1</tt>, is herkend als tor exitnode. Bewerken via tor is niet toegestaan om misbruik te voorkomen.',
+	'right-torunblocked' => 'Automatische blokkades van tor exitnodes omzeilen',
+);
+
+/** Polish (Polski)
+ * @author Beau
+ */
+$messages['pl'] = array(
+	'torblock-desc'      => 'Blokuje możliwość edycji dla użytkowników anonimowych łączących się przez serwery wyjściowe sieci Tor',
+	'torblock-blocked'   => 'Twój adres IP <tt>$1</tt> został automatycznie zidentyfikowany jako serwer wyjściowy sieci Tor. Możliwość edycji z wykorzystaniem tej sieci jest zablokowana w celu zapobiegania nadużyciom.',
+	'right-torunblocked' => 'Obejście automatycznych blokad zakładanych na serwery wyjściowe sieci Tor',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'torblock-desc'      => 'Позволяет блокировать выходные узлы сети Tor',
+	'torblock-blocked'   => 'Ваш IP-адрес, <tt>$1</tt>, был автоматически определён как выходной узел сети Tor.
+Редактирование посредством Tor запрещено во избежание злоупотреблений.',
+	'right-torunblocked' => 'обход автоматической блокировки узлов сети Tor',
 );
 
