b8e44594bb42eb42e88f8110199c74c059aa0033
Sam Reed <reedy@users.mediawiki.org>
2012-01-27 20:05:18 +0000

    Trim trailing whitespace from r105244

--- ASN1Parser.php
+++ ASN1Parser.php
@@ -18,7 +18,7 @@ class ASN1Parser {
 		22 => 'IA5String',
 		23 => 'UTCTime'
 	);
-	
+
 	const INTEGER = 2;
 	const BIT_STRING = 3;
 	const OCTET_STRING = 4;
@@ -30,43 +30,43 @@ class ASN1Parser {
 	const T61String = 20;
 	const IA5String = 22;
 	const UTCTime = 23;
-	
+
 	static function encodeLength($number) {
 		if ($number < 128)
 			return chr($number);
-		
+
 		$out = pack("N*", $number);
 		$out = ltrim( $out, "\0" );
-		
+
 		return chr( 0x80 | strlen($out) ) . $out;
 	}
 
-	static function decode($buffer) {	
+	static function decode($buffer) {
 		if ( strlen( $buffer ) < 2 )
 			throw new ASN1Exception( 'ASN1 string is too short' );
-		
+
 		$i = 0;
 		$result = array();
 		while ( $i < strlen( $buffer ) ) {
 			$item = array();
-			
+
 			$tag = ord( $buffer[$i] );
 			$item['tag-class'] = self::$tagClasses[$tag >> 6];
-			
+
 			$i++;
 			$constructed = $tag & 0x20; // Primitive/Constructed bit
 			$tag &= 0x1f;
-			
+
 			if ( $tag != 0x1f ) {
 				// Great! it's in one octet
-				
+
 				$item['tag'] = $tag;
 			} else {
 				$tag = 0;
 				for (; $i < strlen($buffer); $i++) {
 					$t = ord( $buffer[$i] );
 					$tag = ( $tag << 7 ) | ( $t & 0x7f );
-					
+
 					// The last octet of the tag identifier will have the high-bit set to 0
 					if ( ( $t & 0x80 ) == 0 ) {
 						break;
@@ -78,11 +78,11 @@ class ASN1Parser {
 				$item['tag'] = $tag;
 				$i++;
 			}
-			
+
 			/* Parse length */
 			$length = ord( $buffer[$i] );
 			$i++;
-			
+
 			if ( ( $length & 0x80 ) == 0 ) {
 				$item['length'] = $length;
 			} else {
@@ -91,7 +91,7 @@ class ASN1Parser {
 				if ( strlen( $lengthBytes ) != $l ) {
 					throw new ASN1Exception( 'Not enough bytes for long-form length' );
 				}
-				
+
 				$length = 0;
 				for ($j = 0; $j < $l; $j++, $i++) {
 					$length = ( $length << 8 ) | ord( $lengthBytes[$j] );
@@ -100,41 +100,41 @@ class ASN1Parser {
 						throw new ASN1Exception( 'Overflow calculating length' );
 				}
 				$item['length'] = $length;
-				
+
 			}
 			$item['contents'] = substr( $buffer, $i, $length );
 			$i += $length;
-			
+
 			if ( $constructed ) {
 				$item['contents'] = self::decode( $item['contents'] );
 			} elseif ( $tag == 6 ) {
 				/* We could show the pretty name here instead of the hex dump
-				 * 
+				 *
 				 * a source could be crypto/objects/objects.txt from openssl
-				 * project or crypto/objects/obj_dat.txt (where hexadecimal 
+				 * project or crypto/objects/obj_dat.txt (where hexadecimal
 				 * data is available )
 				 */
 				 $item['contents'] = $item['contents'];
 			} else {
 				$item['contents'] = $item['contents'];
 			}
-			
+
 			$result[] = $item;
 		}
-		
+
 		return count ( $result ) > 1 ? $result : $result[0];
 	}
-	
+
 	/**
 	 * Prettifies an array output by decode()
 	 */
 	static function prettyDecode($decodedArray) {
 		$decoded = $decodedArray;
 		array_walk_recursive($decoded, array( __CLASS__, 'prettyItem' ) );
-		
+
 		return $decoded;
 	}
-	
+
 	static protected function prettyItem(&$value, $key) {
 		switch ($key) {
 			case 'contents': // Not called when contents is an array
@@ -147,7 +147,7 @@ class ASN1Parser {
 				break;
 		}
 	}
-	
+
 	static function buildTag($tagId, $contents) {
 		if ( is_int( $tagId ) &&  $tagId < 31 ) {
 			if ( $tagId == self::SEQUENCE || $tagId == self::SET ) {
@@ -159,7 +159,7 @@ class ASN1Parser {
 		}
 		$out .= self::encodeLength( strlen( $contents ) );
 		$out .= $contents;
-		
+
 		return $out;
 	}
 }
