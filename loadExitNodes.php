b47c638b649b360e5945c07dfacfc5d867074531
Umherirrender <umherirrender_de.wp@web.de>
2018-03-09 22:06:22 +0100

    Fix typo in word compatibility
    Change-Id: I7f6b025dd458a3370d37bcd7096835044dc797a6

--- loadExitNodes.php
+++ loadExitNodes.php
@@ -2,6 +2,6 @@
 
 /**
  * Alias file for renamed maintenance script in version 1.30.
- * Backward compatibilty for cron jobs.
+ * Backward compatibility for cron jobs.
  */
 require_once __DIR__ . '/maintenance/loadExitNodes.php';
