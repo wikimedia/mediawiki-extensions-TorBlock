8e461f953d9fe24816c23da6befa1d17c98d3a0f
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-09-01 15:45:41 +0000

    Soft-deprecate loadExitNodes.php
    Follow-up Ibd6ee1dc.
    
    Bug: T203324
    Change-Id: Ie350a57d2896ced94003c1888f02da3b15d048e6

--- loadExitNodes.php
+++ loadExitNodes.php
@@ -1,7 +1,7 @@
 <?php
-
 /**
  * Alias file for renamed maintenance script in version 1.30.
  * Backward compatibility for cron jobs.
+ * @deprecated since 1.31; please use maintenance/loadExitNodes.php instead.
  */
 require_once __DIR__ . '/maintenance/loadExitNodes.php';
