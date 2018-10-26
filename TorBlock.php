023ea7ebf6735d9ea84d9b488dc45c019b3e4d11
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 22:02:40 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I009d9e5924869f0ef46affc822d199c5bb4b0ac9

--- TorBlock.php
+++ TorBlock.php
@@ -4,11 +4,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'TorBlock' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['TorBlock'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for TorBlock extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the TorBlock extension requires MediaWiki 1.29+' );
