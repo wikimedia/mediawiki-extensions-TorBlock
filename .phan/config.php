11482b0dd16f60a1817d29472ccc5febd5761bed
MarcoAurelio <maurelio@tools.wmflabs.org>
2019-02-24 20:29:07 +0100

    Upgrade TorBlock to use a newer version of phan
    Bug: T216947
    Change-Id: I166c75c14f58ba3e5d0928ec6f612a974bb02f33

--- .phan/config.php
+++ .phan/config.php
@@ -1,17 +1,17 @@
 <?php
-$cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
-		'./../../extensions/AbuseFilter'
+		'../../extensions/AbuseFilter'
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
-		'./../../extensions/AbuseFilter'
+		'../../extensions/AbuseFilter'
 	]
 );
 
--- includes/TorExitNodes.php
+++ includes/TorExitNodes.php
@@ -196,6 +196,7 @@ class TorExitNodes {
 				}
 
 				// Trim surrounding brackets for IPv6 addresses.
+				// @phan-suppress-next-line PhanTypeArraySuspicious false positive
 				$hasBrackets = $ip[0] == '[';
 				if ( $hasBrackets ) {
 					$ip = substr( $ip, 1, -1 );
